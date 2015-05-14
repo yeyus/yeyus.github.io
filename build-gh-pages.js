09bdd0c7789a6b88e7c7c07db44ec0b3db10cb53
Jesus F. Trujillo <elyeyus@gmail.com>
2015-05-13 23:13:53 -0700

    set logger

--- build-gh-pages.js
+++ build-gh-pages.js
@@ -1,4 +1,9 @@
 var ghpages = require('gh-pages');
 var path = require('path');
 
-ghpages.publish(path.join(__dirname, 'dist'), { branch: 'master' });
+ghpages.publish(path.join(__dirname, 'dist'), { 
+	branch: 'master',
+	logger: function(msg) {
+		console.log(msg);
+	}
+});
